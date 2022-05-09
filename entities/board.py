b7d510a96df38edc9b977ebfd045939d8d793b8b
Adicj <petre82adrian@gmail.com>
2022-05-09 20:21:35 +0300

    Spaces

---
+++ entities/board.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+
+
+class Space:
+
+    @abstractmethod
+    def action(self, player):
+        pass
+
+
+class TaxSpace(Space):
+    def action(self, player):
+        player.pay(50)
+
+
+class CornerSpace(Space):
+    def __init__(self, is_free_parking = True, is_start = False, is_go_to_jail = False, is_visit_jail = False):
+        self.is_free_parking = is_free_parking
+        self.is_start = is_start
+        self.is_go_to_jail = is_go_to_jail
+        self.is_visit_jail = is_visit_jail
+
+    def action(self, player):
+        if self.is_start:
+            player.get_paycheck()
+        elif self.is_go_to_jail:
+            player.go_to_jail()
+
+
+class PropertySpace(Space):
+    def __init__(self, property_):
+        self.property = property_
+
+    def action(self, player):
+        if self.property.owner is None:
+            player.buy(self.property)
+        elif self.property.owner is not player:
+            player.pay(self.property.rent, self.property.owner)
